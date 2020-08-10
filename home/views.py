59ca8e89471728c1d1dd924d77763c2369623de7
miracbayraktar <mirabi_61@hotmail.com>
2020-08-10 09:45:17 +0300

    ilk 13 video güncelleme

---
+++ home/views.py
@@ -0,0 +1,46 @@
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from home.models import Setting,ContactForm,ContactFormMessage
+
+
+def index(request):
+
+    setting=Setting.objects.get(pk=1)
+
+    context = {'setting': setting, 'page':'home'}
+    return render(request, 'index.html', context)
+
+def hakkimizda(request):
+
+    setting=Setting.objects.get(pk=1)
+
+    context = {'setting': setting, 'page':'hakkimizda'}
+    return render(request, 'hakkimizda.html', context)
+
+def referanslar(request):
+
+    setting=Setting.objects.get(pk=1)
+
+    context = {'setting': setting, 'page':'referanslar'}
+    return render(request, 'referanslar.html', context)
+
+def iletisim(request):
+
+    if request.method== 'POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir. Teşekkür Ederiz")
+            return HttpResponseRedirect('/iletisim')
+
+    setting=Setting.objects.get(pk=1)
+    form = ContactForm()
+    context = {'setting': setting, 'form':form}
+    return render(request, 'iletisim.html', context)
