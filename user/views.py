ba20a708dd89799ff650570c2259ce6348268d58
miracbayraktar <mirabi_61@hotmail.com>
2020-08-13 23:50:40 +0300

    Kullanıcı Uye Bilgilerini ve Şifresini Güncelleme

--- user/views.py
+++ user/views.py
@@ -1,9 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from home.models import UserProfil
 from product.models import Category
+from user.forms import UserUpdateForm, ProfileUpdateForm
 
 
 def index(request):
@@ -12,3 +16,47 @@ def index(request):
     profile=UserProfil.objects.get(user_id=current_user.id)
     context = {'category': category,'profile': profile}
     return render(request, 'user_profile.html',context)
+
+
+def user_update(request):
+    if request.method=='POST':
+        user_form=UserUpdateForm(request.POST, instance=request.user)
+        profile_form=ProfileUpdateForm(request.POST, request.FILES, instance=request.user.userprofil)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'güncellendi')
+            return  redirect('/user')
+
+    else:
+        category=Category.objects.all()
+        current_user=request.user
+        user_form=UserUpdateForm(instance=request.user)
+        profile_form=ProfileUpdateForm(instance=request.user.userprofil)
+        context = {'category': category,
+                   'user_form': user_form,
+                   'profile_form': profile_form
+        }
+        return render(request, 'user_update.html',context)
+
+
+def change_password(request):
+    if request.method=='POST':
+        form=PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user=form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'password değişti')
+            return  redirect('change_password')
+        else:
+            messages.error(request, 'Lütfen doğru şifre girin.<br>'+ str(form.errors))
+            return HttpResponseRedirect('/user/password')
+
+    else:
+        category=Category.objects.all()
+
+        form=PasswordChangeForm(request.user)
+        context ={'form': form,'category': category}
+
+
+        return render(request, 'change_password.html',context)
