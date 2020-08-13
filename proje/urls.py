a299fb49f96c2dc6e735d035ac40d680229d67d3
miracbayraktar <mirabi_61@hotmail.com>
2020-08-13 14:16:53 +0300

    Anasayfa Uye Paneli ve Profil Bilgileri

--- proje/urls.py
+++ proje/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('iletisim/', views.iletisim, name='iletisim'),
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
+    path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('category/<int:id>/<slug:slug>/', views.category_products, name='category_products'),
@@ -35,6 +36,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout_view'),
     path('login/',views.login_view,name='login_view'),
     path('signup/',views.signup_view,name='signup_view'),
+
 
 
 ]
